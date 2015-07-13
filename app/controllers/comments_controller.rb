3876b74027b91ff4f2a12ddc335b4944f498e674
Chris Keaney <chris.keaney@gmail.com>
2015-07-13 07:43:50 -0500

    Fixed voting bug in model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -23,7 +23,7 @@ end
 def vote
   comment = Comment.find(params[:id])
   vote = Vote.create(voteable: comment, user: current_user, vote: params[:vote])
-  
+  #binding.pry
   if vote.valid?
     flash[:notice] = "Your vote was counted."
   else
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,7 @@ class Vote < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :voteable, polymorphic: true
 
-	validates_uniqueness_of :user, scope: :voteable
+	validates_uniqueness_of :user, scope: [:voteable_id, :voteable_type]
+	
 
 end
